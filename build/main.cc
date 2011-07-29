a43c7b5126f2ac574222b21428e7572d3bc403f8
Elijah Insua <tmpvar@gmail.com>
2011-07-28 21:32:02 -0700

    next step, compile shaders

--- build/main.cc
+++ build/main.cc
@@ -1,83 +1,93 @@
 #include "../src/WebGLRenderingContext.h"
 #include <iostream>
+#include <assert.h>
+
+void addShader(WebGLRenderingContext *gl, WebGLProgram program, GLenum type, DOMString *source) {
+  WebGLShader shader = gl->createShader(type);
+  gl->shaderSource(shader, *source);
+  gl->compileShader(shader);
+
+  assert(gl->getShaderParameter(shader, gl->COMPILE_STATUS));
+
+  gl->attachShader(program, shader);
+}
 
 int main() {
-  WebGLRenderingContext *context = new WebGLRenderingContext();
+  WebGLRenderingContext *gl = new WebGLRenderingContext();
 
-  //WebGLProgram shader = context->createProgram();
+  WebGLProgram program = gl->createProgram();
 
-/*  function shaderProgram(gl, vs, fs) {
-  	var prog = gl.createProgram();
-  	var addshader = function(type, source) {
-  		var s = gl.createShader((type == 'vertex') ?
-  			gl.VERTEX_SHADER : gl.FRAGMENT_SHADER);
-  		gl.shaderSource(s, source);
-  		gl.compileShader(s);
-  		if (!gl.getShaderParameter(s, gl.COMPILE_STATUS)) {
-  			throw "Could not compile "+type+
-  				" shader:\n\n"+gl.getShaderInfoLog(s);
-  		}
-  		gl.attachShader(prog, s);
-  	};
-  	addshader('vertex', vs);
-  	addshader('fragment', fs);
-  	gl.linkProgram(prog);
-  	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-  		throw "Could not link the shader program!";
-  	}
-  	return prog;
-  }
+  DOMString *vertex_shader = new DOMString();
+  /*
+  "attribute vec3 pos;"+
+  "void main() {"+
+  "  gl_Position = vec4(pos, 2.0);"+
+  "}"
+  */
+  
+  DOMString *fragment_shader = new DOMString();
+  /*"void main() {"+
+  "  gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);"+
+  "}"*/
 
+  addShader(gl, program, gl->VERTEX_SHADER, vertex_shader);
+  addShader(gl, program, gl->FRAGMENT_SHADER, fragment_shader);
+  
+  gl->linkProgram(program);
+  
+  assert(gl->getProgramParameter(program, gl->LINK_STATUS));
+
+/*
   function attributeSetFloats(gl, prog, attr_name, rsize, arr) {
-  	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-  	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr),
-  		gl.STATIC_DRAW);
-  	var attr = gl.getAttribLocation(prog, attr_name);
-  	gl.enableVertexAttribArray(attr);
-  	gl.vertexAttribPointer(attr, rsize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr),
+      gl.STATIC_DRAW);
+    var attr = gl.getAttribLocation(prog, attr_name);
+    gl.enableVertexAttribArray(attr);
+    gl.vertexAttribPointer(attr, rsize, gl.FLOAT, false, 0, 0);
   }
 
   function draw() {
-  	try {
-  		var gl = document.getElementById("webgl")
-  			.getContext("experimental-webgl");
-  		if (!gl) { throw "x"; }
-  	} catch (err) {
-  		throw "Your web browser does not support WebGL!";
-  	}
-  	gl.clearColor(0.8, 0.8, 0.8, 1);
-  	gl.clear(gl.COLOR_BUFFER_BIT);
+    try {
+      var gl = document.getElementById("webgl")
+        .getContext("experimental-webgl");
+      if (!gl) { throw "x"; }
+    } catch (err) {
+      throw "Your web browser does not support WebGL!";
+    }
+    gl.clearColor(0.8, 0.8, 0.8, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  	var prog = shaderProgram(gl,
-  		"attribute vec3 pos;"+
-  		"void main() {"+
-  		"	gl_Position = vec4(pos, 2.0);"+
-  		"}",
-  		"void main() {"+
-  		"	gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);"+
-  		"}"
-  	);
-  	gl.useProgram(prog);
+    var prog = shaderProgram(gl,
+      "attribute vec3 pos;"+
+      "void main() {"+
+      "  gl_Position = vec4(pos, 2.0);"+
+      "}",
+      "void main() {"+
+      "  gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);"+
+      "}"
+    );
+    gl.useProgram(prog);
 
-  	attributeSetFloats(gl, prog, "pos", 3, [
-  		-1, 0, 0,
-  		0, 1, 0,
-  		0, -1, 0,
-  		1, 0, 0
-  	]);
+    attributeSetFloats(gl, prog, "pos", 3, [
+      -1, 0, 0,
+      0, 1, 0,
+      0, -1, 0,
+      1, 0, 0
+    ]);
 
-  	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   }
 
   function init() {
-  	try {
-  		draw();
-  	} catch (e) {
-  		alert("Error: "+e);
-  	}
+    try {
+      draw();
+    } catch (e) {
+      alert("Error: "+e);
+    }
   }
   setTimeout(init, 100);*/
   
-  
+  std::cout << "Done!" << std::endl;
   return 0;
 }
