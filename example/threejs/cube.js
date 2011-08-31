5ee0fa1c4f88677bddb09b65b5d1e80a5802f0de
Elijah Insua <tmpvar@gmail.com>
2011-08-30 18:13:20 -0700

    three.js now works (in some capacity)

--- example/threejs/cube.js
+++ example/threejs/cube.js
@@ -29,7 +29,7 @@ var windowHalfY = 150;
 
 camera = new THREE.Camera( 70, w / h, 1, 1000 );
 camera.position.y = 150;
-camera.position.z = 500;
+camera.position.z = 100;
 camera.target.position.y = 150;
 
 scene = new THREE.Scene();
@@ -37,11 +37,8 @@ scene = new THREE.Scene();
 
 var sphereMaterial = new THREE.MeshLambertMaterial(
 {
-    color: 0xCC0000
+    color: 0xFFFFFF
 });
-
-// set up the sphere vars
-var radius = 50, segments = 16, rings = 16;
 
 // create a new mesh with sphere geometry -
 // we will cover the sphereMaterial next!
@@ -54,6 +51,18 @@ scene.addChild(sphere);
 
 // Cube
 sphere.position.y = 150;
+
+
+// create a point light
+var pointLight = new THREE.PointLight( 0xFF0000 );
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 100;
+
+// add to the scene
+scene.addLight(pointLight);
 
 
 renderer = new THREE.WebGLRenderer();
--- example/threejs/getting-started.js
+++ example/threejs/getting-started.js
@@ -21,80 +21,67 @@ var WebGLRenderingContext = require('../lib/webgl.js').WebGLRenderingContext;
 var THREE = require('../example/threejs/three.js');
 
 THREE.WebGLRenderer.prototype = {};
-// set the scene size
-var WIDTH = 400,
-    HEIGHT = 300;
 
-// set some camera attributes
-var VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 0.1,
-    FAR = 10000;
 
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
-//var $container = $('#container');
+  // set the scene size
+  var WIDTH = 300,
+      HEIGHT = 300;
 
-// create a WebGL renderer, camera
-// and a scene
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.Camera(  VIEW_ANGLE,
-                                ASPECT,
-                                NEAR,
-                                FAR  );
-var scene = new THREE.Scene();
+  // set some camera attributes
+  var VIEW_ANGLE = 45,
+      ASPECT = WIDTH / HEIGHT,
+      NEAR = 0.1,
+      FAR = 10000;
 
-// the camera starts at 0,0,0 so pull it back
-camera.position.z = 300;
 
-// start the renderer
-renderer.setSize(WIDTH, HEIGHT);
+  // create a WebGL renderer, camera
+  // and a scene
+  var renderer = new THREE.WebGLRenderer();
+  var camera = new THREE.Camera(  VIEW_ANGLE,
+                                  ASPECT,
+                                  NEAR,
+                                  FAR  );
+  var scene = new THREE.Scene();
 
-// attach the render-supplied DOM element
-//$container.append(renderer.domElement);
+  // the camera starts at 0,0,0 so pull it back
+  camera.position.z = 300;
 
-// create the sphere's material
-var sphereMaterial = new THREE.MeshLambertMaterial(
-{
-    color: 0xCC0000
-});
+  // start the renderer
+  renderer.setSize(WIDTH, HEIGHT);
 
-// set up the sphere vars
-var radius = 50, segments = 16, rings = 16;
 
-// create a new mesh with sphere geometry -
-// we will cover the sphereMaterial next!
-var sphere = new THREE.Mesh(
-   new THREE.SphereGeometry(radius, segments, rings),
-   sphereMaterial);
+  // create the sphere's material
+  var sphereMaterial = new THREE.MeshLambertMaterial(
+  {
+      color: 0xCC0000
+  });
 
-// add the sphere to the scene
-scene.addChild(sphere);
+  // set up the sphere vars
+  var radius = 50, segments = 16, rings = 16;
 
-// create a point light
-var pointLight = new THREE.PointLight( 0xFFFFFF );
+  // create a new mesh with sphere geometry -
+  // we will cover the sphereMaterial next!
+  var sphere = new THREE.Mesh(
+     new THREE.SphereGeometry(radius, segments, rings),
+     sphereMaterial);
 
-// set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
+  // add the sphere to the scene
+  scene.addChild(sphere);
 
-// add to the scene
-scene.addLight(pointLight);
+  // create a point light
+  var pointLight = new THREE.PointLight( 0xFFFFFF );
 
-function render() {
-  renderer.render(scene, camera);
-}
+  // set its position
+  pointLight.position.x = 10;
+  pointLight.position.y = 50;
+  pointLight.position.z = 130;
+
+  // add to the scene
+  scene.addLight(pointLight);
 
 // draw!
-var a = 30;
+var a = 1000;
 while(a--) {
-  try {
-    render();
-  } catch (e) {
-    console.log(e.stack);
-    fail();
-  }
+  renderer.render(scene, camera);
   webgl_rendering_context_flush();
-  webgl_rendering_context_finish();
 }
